ae4c33f06963fa273eae06f55ff7ec3c87f0878a
wiyye <guillermogarciadiaz7@gmail.com>
2021-11-17 18:21:11 +0000

    nombre del commit

--- convertir bin-dec-hex.py
+++ convertir bin-dec-hex.py
@@ -2,7 +2,7 @@ valoresHexadecimal=['0xa076407d3f7440000', '0x140ec80fa7ee880000',
 '0x1e162c177be5cc0000', '0x281d901f4fdd100000', 
 '0x3224f42723d4540000', '0x3c2c582ef7cb980000', 
 '0x4633bc36cbc2dc0000', '0x503b203e9fba200000', 
-'0x5a42844673b1640000', '0x6449e84e47a8a80000', '0x6e514c561b9fec0000', ]
+'0x5a42844673b1640000', '0x6449e84e47a8a80000', '0x6e514c561b9fec0000']
 
 numeroBinario='1001010011'
 decimal=int(numeroBinario,2)
@@ -29,7 +29,8 @@ paresValores=zip(valoresHexadecimal, listaDecimales)
 paresValores=list(paresValores)
 print(paresValores)
 
-
+'''
 Diccionario=dict(paresValores)
 print('***********************')
 print(Diccionario)
+'''
